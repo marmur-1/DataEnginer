21b0124ce904c6986000904316494733ed0b7faf
marmur <wm-kink-1@yandex.ru>
2022-12-03 17:18:18 +1100

    Первый DAGs

---
+++ 3/3_6/dags/first.py
@@ -0,0 +1,13 @@
+#первый тестовый DAGs
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+def hello():
+    print("Airflow")
+
+with DAG(dag_id="first_dag",start_date=datetime(2022,1,1),schedule="0 0 * * *") as dag:
+    bash_task = BashOperator(task_id="hello",bash_command="echo hello")
+    python_task = PythonOperator(task_id="world", python_callable = hello)
+    bash_task >> python_task
