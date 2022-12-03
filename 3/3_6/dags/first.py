0d1d0aa874e424a906863ae50be449e8769e4760
marmur <wm-kink-1@yandex.ru>
2022-12-03 20:25:26 +1100

    Добавление логов для проверки

--- 3/3_6/dags/first.py
+++ 3/3_6/dags/first.py
@@ -7,7 +7,7 @@ from airflow.operators.python_operator import PythonOperator
 def hello():
     print("Airflow")
 
-with DAG(dag_id="first_dag",start_date=datetime(2022,1,1),schedule="0 0 * * *") as dag:
+with DAG(dag_id="first_dag",start_date=datetime(2022,12,3),schedule="0 0 * * *") as dag:
     bash_task = BashOperator(task_id="hello",bash_command="echo hello")
     python_task = PythonOperator(task_id="world", python_callable = hello)
     bash_task >> python_task
