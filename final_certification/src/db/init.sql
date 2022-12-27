96cad9ba84b240cf81cb75b5d3a7636bcc67b593
marmur-1 <55779496+marmur-1@users.noreply.github.com>
2022-12-28 08:54:47 +1100

    Форматирование текста

--- final_certification/src/db/init.sql
+++ final_certification/src/db/init.sql
@@ -2,23 +2,23 @@ CREATE DATABASE project;
 GRANT ALL PRIVILEGES ON DATABASE project TO airflow;
 \c project;
 
-CREATE TABLE IF NOT EXISTS tickets(	    -- Таблица тикетов валюты
+CREATE TABLE IF NOT EXISTS tickets(			-- Таблица тикетов валюты
 	symbol			text NOT NULL, 		-- символ акции
 	timestamp		timestamp NOT NULL,	-- дата и время тикета
-    id_day          text,               -- сурогатный ключ категории        
-	open			float,				-- цена открытия
-    close			float,				-- цена закрытия
-    high			float,				-- максимальная цена
-    low				float,				-- минимальная цена
-    volume			int,				-- колличество торгов
+	id_day			text,               	-- сурогатный ключ категории        
+	open			float,			-- цена открытия
+	close			float,			-- цена закрытия
+	high			float,			-- максимальная цена
+	low			float,			-- минимальная цена
+	volume			int,			-- колличество торгов
 	PRIMARY KEY (symbol, timestamp)
 );
 
-CREATE FUNCTION public.tickets_id_day() -- тригерная функция добавления сурогатного ключа
-    RETURNS trigger
-    LANGUAGE 'plpgsql'
-    VOLATILE
-    COST 100
+CREATE FUNCTION public.tickets_id_day() 		-- тригерная функция добавления сурогатного ключа
+	RETURNS trigger
+	LANGUAGE 'plpgsql'
+	VOLATILE
+	COST 100
 AS $BODY$
 BEGIN
 	NEW.id_day := concat(NEW.timestamp::date,'_',NEW.symbol);
@@ -26,18 +26,18 @@ BEGIN
 END
 $BODY$;
 
-CREATE TRIGGER tickets_id_day           -- сам тригер
+CREATE TRIGGER tickets_id_day           		-- сам тригер
 BEFORE INSERT ON tickets FOR EACH ROW
 EXECUTE PROCEDURE public.tickets_id_day();
 
-CREATE TABLE IF NOT EXISTS days_analytics(          -- Таблица аналитики дня
-    id_day              text NOT NULL PRIMARY KEY,  -- Суррогатный ключ категории    
-	symbol			    text NOT NULL, 		        -- Название валюты
-    sum_volume		    int,			            -- Суммарный объем торгов за последние сутки
-	day_open		    float,				        -- Курс валюты на момент открытия торгов для данных суток
-    day_close		    float,				        -- Курс валюты на момент закрытия торгов для данных суток
-    relative_op		    float,                      -- Разница(в %) курса с момента открытия до момента закрытия торгов для данных суток
-    max_volume_interval timestamp,                  -- Минимальный временной интервал, на котором был зафиксирован самый крупный объем торгов для данных суток
-    max_price_interval	timestamp,				    -- Минимальный временной интервал, на котором был зафиксирован максимальный курс для данных суток
-    min_price_interval	timestamp				    -- Минимальный временной интервал, на котором был зафиксирован минимальный курс торгов для данных суток
+CREATE TABLE IF NOT EXISTS days_analytics(          	-- Таблица аналитики дня
+	id_day			text NOT NULL PRIMARY KEY,-- Суррогатный ключ категории    
+	symbol			text NOT NULL, 		-- Название валюты
+	sum_volume		int,			-- Суммарный объем торгов за последние сутки
+	day_open		float,			-- Курс валюты на момент открытия торгов для данных суток
+	day_close		float,			-- Курс валюты на момент закрытия торгов для данных суток
+	relative_op		float,			-- Разница(в %) курса с момента открытия до момента закрытия торгов для данных суток
+	max_volume_interval 	timestamp,		-- Минимальный временной интервал, на котором был зафиксирован самый крупный объем торгов для данных суток
+	max_price_interval	timestamp,		-- Минимальный временной интервал, на котором был зафиксирован максимальный курс для данных суток
+	min_price_interval	timestamp		-- Минимальный временной интервал, на котором был зафиксирован минимальный курс торгов для данных суток
 );
